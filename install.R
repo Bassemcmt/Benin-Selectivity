8cdb6e957e7b573e9ca06ff1ff17afcf2c8e162d
Bassem CHERMITI <bassemcmt@gmail.com>
2024-05-12 21:56:13 +0100

    Create install.R

---
+++ install.R
@@ -0,0 +1,11 @@
+# Define the list of packages
+packages <- c(
+  "rsconnect", "shiny", "shinythemes", "tidyverse", "lubridate",
+  "ggplot2", "dplyr", "DT", "png", "ggiraph", "RColorBrewer",
+  "readr", "shinyWidgets", "shinymanager", "formattable", "writexl",
+  "WriteXLS", "rpivotTable", "htmlwidgets", "shinyjs", "clipr", 
+  "rvest", "shinydashboard", "ggrepel", "readxl", "plotly", "shinyalert"
+)
+
+# Install packages
+install.packages(packages)
