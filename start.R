a230cbd428da953e7fe97407e77fe806519468b5
Bassem CHERMITI <bassemcmt@gmail.com>
2024-05-12 23:07:10 +0100

    Update start.R

--- start.R
+++ start.R
@@ -1,2 +1,16 @@
-shiny::runGitHub("Benin-Selectivity", "bassemcmt", subdir = "app.r")
+# Charger la bibliothèque shiny
+library(shiny)
+
+# Fonction pour démarrer l'application Shiny
+runMyApp <- function() {
+  # Spécifier le dépôt et le fichier Shiny à exécuter
+  repo <- "bassemcmt/Benin-Selectivity"  # Nom du dépôt GitHub
+  app_file <- "app.r"  # Nom du fichier Shiny à exécuter
+
+  # Exécuter l'application Shiny à partir du dépôt GitHub
+  shiny::runGitHub(repo, app_file)
+}
+
+# Appeler la fonction pour démarrer l'application
+runMyApp()
 
